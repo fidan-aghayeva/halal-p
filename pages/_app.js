4e45eba810155edb0ed7f6f7d7329d162b3be075
Fidan Mirizade <fidan.mirizadaa@gmail.com>
2023-01-05 20:26:48 +0400

    initialaze redux-toolkit, create global slice

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,37 @@
-import '../src/styles/globals.css'
+import { Provider } from 'react-redux';
+import { enableES5 } from 'immer';
+import App from 'next/app';
+import GlobalContainer from 'components/GlobalContainer';
+import Renderer from 'utils/Renderer';
+import { detectSSRDevice } from 'utils/device-detection';
+import { globalActions } from 'redux/slices/global';
+import store from 'redux/store';
+
+enableES5();
+
+import 'styles/index.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    const getLayout = Component.getLayout || (page => page);
+
+    return (
+        <Provider store={store}>
+            <GlobalContainer>{getLayout(<Component {...pageProps} />)}</GlobalContainer>
+        </Provider>
+    );
 }
 
-export default MyApp
+MyApp.getInitialProps = async context => {
+    const { req } = context;
+
+    if (Renderer.isServerRender(req)) {
+        const userAgent = req.headers['user-agent'];
+        const currentDevice = detectSSRDevice(userAgent);
+
+        store.dispatch(globalActions.setCurrentDevice(currentDevice));
+    }
+
+    return { props: {} };
+};
+
+export default MyApp;
