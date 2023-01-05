a4e43e7fd1cd6fc5052d39a843255b72a954dcee
Fidan Mirizade <fidan.mirizadaa@gmail.com>
2023-01-05 20:19:00 +0400

    add icons

---
+++ src/assets/icons/index.js
@@ -0,0 +1,11 @@
+import Instagram from './instagram.svg';
+import Facebook from './facebook.svg';
+import Linkedin from './linkedin.svg';
+import Logo from './logo.svg';
+import WhiteLogo from './logo-white.svg';
+
+export const InstagramIcon = Instagram;
+export const FacebookIcon = Facebook;
+export const LinkedinIcon = Linkedin;
+export const LogoIcon = Logo;
+export const WhiteLogoIcon = WhiteLogo;
