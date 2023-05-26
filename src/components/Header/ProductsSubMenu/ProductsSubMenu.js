5f5c0d22c35aacd9ce7b399bdc8a86fdf344f62e
fidanmrzd <fidan.mirizadaa@gmail.com>
2023-05-26 18:38:38 +0400

    header products submenu changes

--- src/components/Header/ProductsSubMenu/ProductsSubMenu.js
+++ src/components/Header/ProductsSubMenu/ProductsSubMenu.js
@@ -1,17 +1,29 @@
+import { useSelector } from 'react-redux';
 import Link from 'next/link';
-import productCategories from './mock';
 
 import styles from './ProductsSubMenu.module.scss';
 
 const ProductsSubMenu = () => {
+    const { sections, categories } = useSelector(state => state.global);
+
     return (
         <div className={styles.container}>
-            {productCategories.map(category => (
-                <div key={category.id} className={styles.category}>
-                    <Link href={'/'} className={styles.categoryLink}>{category.name}</Link>
-                    {category.subCategories.map(subCategory => (
-                        <Link href={'/category'} key={subCategory.id} className={styles.subCategoryLink}>{subCategory.name}</Link>
-                    ))}
+            {sections.map(section => (
+                <div key={section.id} className={styles.category}>
+                    <Link href={`/products/${section.slug}/${section.id}?page=1`} className={styles.categoryLink}>
+                        {section.name}
+                    </Link>
+                    {categories
+                        .filter(category => category.sectionId === section.id)
+                        .map(subCategory => (
+                            <Link
+                                href={`/products/${subCategory.slug}/${subCategory.id}?page=1`}
+                                key={subCategory.id}
+                                className={styles.subCategoryLink}
+                            >
+                                {subCategory.name}
+                            </Link>
+                        ))}
                 </div>
             ))}
         </div>
