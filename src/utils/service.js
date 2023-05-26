91060c50d2330b0440cd9f37b4da492ddeb6e3a4
fidanmrzd <fidan.mirizadaa@gmail.com>
2023-05-26 18:25:07 +0400

    get methods

--- src/utils/service.js
+++ src/utils/service.js
@@ -2,25 +2,131 @@ import axios from 'axios';
 import { SERVICE_URL } from './constants';
 
 export const getHomeSliders = async lang => {
-    const data = await axios.get(`${SERVICE_URL}/${lang}/sliders`);
+    try {
+        const data = await axios.get(`${SERVICE_URL}/${lang}/sliders`);
 
-    return data.data;
+        return data.data;
+    } catch (error) {
+        console.log(error);
+    }
 };
 
-export const getHomeBLogsByType = async ({ lang, type }) => {
-    const data = await axios.get(`${SERVICE_URL}/${lang}/blogs/${type}/main-page`);
+export const getHomeBlogsByType = async ({ lang, type }) => {
+    try {
+        const data = await axios.get(`${SERVICE_URL}/${lang}/blogs/${type}/main-page`);
 
-    return data.data;
+        return data.data;
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 export const getSections = async lang => {
-    const data = await axios.get(`${SERVICE_URL}/${lang}/sections`);
+    try {
+        const data = await axios.get(`${SERVICE_URL}/${lang}/sections`);
 
-    return data.data;
+        return data.data;
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 export const getBlogsDataByType = async ({ lang, type, ...queryParams }) => {
-    const data = await axios.get(`${SERVICE_URL}/${lang}/blogs/${type}`, { params: queryParams });
+    try {
+        const data = await axios.get(`${SERVICE_URL}/${lang}/blogs/${type}`, { params: queryParams });
 
-    return data.data;
+        return data.data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const getBlogsDataByTypeAndId = async ({ lang, type, id }) => {
+    try {
+        const data = await axios.get(`${SERVICE_URL}/${lang}/blogs/${type}/${id}`);
+
+        return data.data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const getEmployees = async lang => {
+    try {
+        const data = await axios.get(`${SERVICE_URL}/${lang}/employees`);
+
+        return data.data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const getCustomers = async type => {
+    try {
+        const data = await axios.get(`${SERVICE_URL}/az/customers`, { params: { type } });
+
+        return data.data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const getContacts = async lang => {
+    try {
+        const data = await axios.get(`${SERVICE_URL}/${lang}/contacts`);
+
+        return data.data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const getPageDescriptionByType = async ({ lang, type }) => {
+    try {
+        const data = await axios.get(`${SERVICE_URL}/${lang}/companies/page-desc`, { params: { type } });
+
+        return data.data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const getMainSiteData = async lang => {
+    try {
+        const data = await axios.get(`${SERVICE_URL}/${lang}/companies`);
+
+        return data.data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const getCategories = async lang => {
+    try {
+        const data = await axios.get(`${SERVICE_URL}/${lang}/categories`);
+
+        return data.data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const getProducts = async ({ lang, ...params }) => {
+    try {
+        const data = await axios.get(`${SERVICE_URL}/${lang}/products`, { params });
+
+        return data.data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const getOurAdvantages = async lang => {
+    try {
+        const data = await axios.get(`${SERVICE_URL}/${lang}/differences`);
+
+        return data.data;
+    } catch (error) {
+        console.log(error);
+    }
 };
