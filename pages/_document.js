0b429922a0837ac925ef2f835bc3865cab3f2054
fidanmrzd <fidan.mirizadaa@gmail.com>
2023-06-12 14:30:38 +0400

    add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,7 @@ class MyDocument extends Document {
                 <Head>
                     <link rel='preconnect' href='https://fonts.googleapis.com' />
                     <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='' />
+                    <link rel='icon' type='image/x-icon' href='/images/favicon.png' />
                     <link
                         href='https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&display=swap'
                         rel='stylesheet'
