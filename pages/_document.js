7579a6f3c0a7b872a43245ea91a67a609a247c1f
Fidan Mirizade <fidan.mirizadaa@gmail.com>
2024-02-14 19:49:13 +0100

    add google tag manager

--- pages/_document.js
+++ pages/_document.js
@@ -19,8 +19,22 @@ class MyDocument extends Document {
                         href='https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&display=swap'
                         rel='stylesheet'
                     />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-P47Q9BBR');`,
+                        }}
+                    />
                 </Head>
                 <body>
+                    <noscript
+                        dangerouslySetInnerHTML={{
+                            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P47Q9BBR" height="0" width="0" style="display: none; visibility: hidden;" />`,
+                        }}
+                    />
                     <Main />
                     <NextScript />
                 </body>
